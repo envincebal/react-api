5e643af04df28c761129eca9eade4ef46fafe610
envincebal <vincentyan1@gmail.com>
2021-03-07 21:05:53 -0800

    Added PhotoItem component.

--- src/components/PhotoItem/PhotoItem.js
+++ src/components/PhotoItem/PhotoItem.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const PhotoItem = () => {
+const PhotoItem = ({photo}) => {
   return (
     <div>
-      photo item
+      <div className="photo-img-container">
+        <img className="photo-img" src={photo.urls.thumb} alt={photo.urls.alt_description} />
+      </div>
+      <div className="photo-content">
+        <p className="photo-time">{photo.created_at}</p>
+        <p className="photo-time">{photo.updated_at}</p>
+        <p className="photo-likes">{photo.likes}</p>
+        <p className="photo-user">{photo.user.name}</p>
+      </div>
+      <div className="photo-buttons">
+        <a className="photo-btn" href={photo.user.links.html}>{photo.user.name}</a>
+        <a className="photo-btn" href={photo.links.html}>View Image</a>
+      </div>
     </div>
   )
 }
--- src/components/PhotoList/PhotoList.js
+++ src/components/PhotoList/PhotoList.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import axios from "axios";
+import PhotoItem from "../PhotoItem/PhotoItem";
 const API_KEY = "oi1hSplSQS_hI67jQphH7UyZTPrpDo61GLyja7nCFeY";
+
 class PhotoList extends Component {
   constructor() {
     super();
@@ -9,21 +11,30 @@ class PhotoList extends Component {
     }
   }
 
-  componentDidMount = async () => {
+  componentDidMount = async() => {
 
-    const data = await axios.get("https://api.unsplash.com/photos",{
+    const {data: photos} = await axios.get("https://api.unsplash.com/photos", {
       headers: {
         Authorization: "Client-ID " + API_KEY
       }
     });
 
-    console.log(data);
+    this.setState({photos})
   }
 
   render() {
-    return (
+    if (this.state.photos.length === 0) 
+      return <div className="loading">Loading</div>
+    
+      return (
       <div>
-        <h1>Hello</h1>
+      {
+        this.state.photos.map(photo => (
+          <PhotoItem 
+            key={photo.id}
+            photo={photo} />
+        ))
+      }
       </div>
     )
 
